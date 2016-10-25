822d8ed29210bb46722d811f9070bb4e0de72017
substancex <wjeanmarie@bklawtech.com>
2016-10-25 21:58:35 +0000

    roman numerals needs debugging

--- js/roman.js
+++ js/roman.js
@@ -16,11 +16,71 @@ $(function() {
   // 4   | IV
   // 1   | I
 
-  function convertToRomanNumeral(number) {
-    console.log("Number = " + number);
+  function convertToRomanNumeral(input) {
+    console.log("Number = " + input);
     var output = "";
+    while (input >= 0) {
+      if (input >= 100) {
+        output = output.concat('C');
+        input -= 100;
+      }else if (input >= 90) {
+        output = output.concat('XC');
+        input -= 90;
+      }else if (input >= 50) {
+        output = output.concat('L');
+        input -= 50;
+      }else if (input >= 40) {
+        output = output.concat('XL');
+        input -= 40;
+      }else if (input >= 10) {
+        output = output.concat('X');
+        input -= 10;
+      }else if (input >= 9) {
+        output = output.concat('IX');
+        input -= 9;
+      }else if (input >= 5) {
+        output = output.concat('V');
+        input -= 5;
+      }else if (input >= 4) {
+        output = output.concat('IV');
+        input -= 4;
+      }else if (input >= 1) {
+        output = output.concat('I');
+        input -= 1;
+      }
+    }
     return output;
   }
+  //   if ( number == 0){
+  //     output = "NULL, 0 dosent count! CHEATER! :(";
+  //   }else if ( number == 1) {
+  //     output = "I";
+  //   }else if ( number == 2) {
+  //     output = "II";
+  //   }else if ( number == 3) {
+  //     output = "III";
+  //   }else if ( number == 4) {
+  //     output = "IV";
+  //   }else if ( number == 5) {
+  //     output = "V";
+  //   }else if ( number == 6) {
+  //     output = "VI";
+  // //}else if ( number == 7) {
+  // //output = "VII";
+  // //}else if ( number == 8) {
+  // //output = "VIII";
+  // //}else if ( number == 9) {
+  // //  output = "IX";
+  // //}else if ( number == 10) {
+  // // output = "X";
+  // //  }else if ( number > 11) {
+  // //    output = "That number is too large!";
+  
+  // //}else {
+  // //  output = "What part of NUMBER do you not understand?"
+  // // }
+  // //  return output;
+  // //}
 
 
 
